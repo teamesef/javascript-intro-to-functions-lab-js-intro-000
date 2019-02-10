7079a2395825b8d4169d219a450d6c71fb1d4ea4
Teamese Felix <teamesef@gmail.com>
2019-02-10 01:08:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase) {
-    return "I can't hear you!"
+    return 'I can\'t hear you!'
   } else if (string.toUpperCase) {
-    return "YES INDEED!"
+    return 'YES INDEED!'
   }
 }
 
