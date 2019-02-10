f9949206ffa90216661e377738b17f0010268659
Teamese Felix <teamesef@gmail.com>
2019-02-10 01:29:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === 'hello') {
+  if (string.toLowerCase()) {
     return 'I can\'t hear you!'
-  } else if (string === 'HELLO') {
+  } else if (string.toUpperCase()) {
     return 'YES INDEED!'
   }
 }
