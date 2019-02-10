d815609d35b8d939c2c8a3b760911345940e3dd2
Teamese Felix <teamesef@gmail.com>
2019-02-10 01:35:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return 'YES INDEED!'
   }
-}
 
   if (string === "I love you, Grandma.") {
     return "I love you, too."
